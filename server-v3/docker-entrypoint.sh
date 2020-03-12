4de9703b6510d7dd04f55493fe1df688330ccd92
kjhricky <kjhricky@gmail.com>
2020-03-12 16:28:29 +0900

    add babel compile logic

--- server-v3/docker-entrypoint.sh
+++ server-v3/docker-entrypoint.sh
@@ -1,11 +1,15 @@
 # dockerize -wait tcp://postgre:5432 -timeout 20s
 
+echo "installing babel"
+npm run babel-version
+npx babel src --out-dir dist
+# npx babel src --watch --out-dir dist
 echo "Waiting for postgres..."
 while ! nc -z postgres-114 5432; do
     sleep 0.1
 done
 # sleep 2
-# ./node_modules/.bin/sequelize db:migrate
+./node_modules/.bin/sequelize db:migrate --env development
 # ./node_modules/.bin/sequelize db:seed:all
 echo "Server started"
-node index.js
+node dist/index.js
